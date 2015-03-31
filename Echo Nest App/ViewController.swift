afbb611f7e104d641ec46fe37272ae56f9d267db
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2015-03-30 20:51:30 -0400

    Initial Commit

---
+++ Echo Nest App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Echo Nest App
+//
+//  Created by Scott Campbell on 2015-03-30.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
